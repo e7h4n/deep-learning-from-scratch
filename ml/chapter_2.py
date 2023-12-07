1f0e84127c0954f7f79f582e93d1e79388542d4c
zhangyc <zhangyc@kanyun.com>
2023-12-07 10:57:00 +0800

    add chapter2

--- ml/chapter_2.py
+++ ml/chapter_2.py
@@ -1,7 +1,43 @@
-def AND(x1, x2):
+import numpy as np
+
+
+def AND_1(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = w1 * x1 + w2 * x2
     if tmp <= theta:
         return 0
     elif tmp > theta:
+        return 1
+
+
+def AND(x1, x2):
+    x = np.array([x1, x2])  # 输入向量
+    w = np.array([0.5, 0.5])  # 权重矩阵？
+    b = -0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])  # 与AND不同的地方
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
         return 1
--- tests/test_chapter_2.py
+++ tests/test_chapter_2.py
@@ -1,7 +1,31 @@
-from ml.chapter_2 import AND
+from ml.chapter_2 import AND, NAND, OR
+
 
 def test_2_3_1():
     assert AND(0, 0) == 0
     assert AND(1, 0) == 0
     assert AND(0, 1) == 0
     assert AND(1, 1) == 1
+
+
+def test_2_3_2():
+    import numpy as np
+
+    x = np.array([0, 1])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    print(w * x)
+    print(np.sum(w * x))
+    print(np.sum(w * x) + b)
+
+
+def test_2_3_3():
+    assert NAND(0, 0) == 1
+    assert NAND(1, 0) == 1
+    assert NAND(0, 1) == 1
+    assert NAND(1, 1) == 0
+
+    assert OR(0, 0) == 0
+    assert OR(1, 0) == 1
+    assert OR(0, 1) == 1
+    assert OR(1, 1) == 1
